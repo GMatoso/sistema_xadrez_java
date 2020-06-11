faac1affe3eb68955692629dac4ea9615140a201
Gabriel Matoso <gabrielmatoso.al@gmail.com>
2020-06-11 02:01:40 -0300

    Primeira classe: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá Mundo!");
+		Position poss =  new Position(3, 5);
+		
+		System.out.println("Position: " + poss.toString());
 	}
 
 }
