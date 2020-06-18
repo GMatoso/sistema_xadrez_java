72c1422e91fcac2c5de2dc2909eced07c924cb4d
Gabriel Matoso <gabrielmatoso.al@gmail.com>
2020-06-18 11:48:55 -0300

    Tratando execeções e limpando a tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -27,19 +27,19 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
-		}
-		catch (RuntimeException e){
+		} 
+		catch (RuntimeException e) {
 			throw new InputMismatchException("Erro ao ler posição. Só é valido valores de a1 até h8.");
 		}
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -51,17 +51,20 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
-		}
+		} 
+		else if (piece.getColor() == Color.WHITE) {
+			System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+		} 
 		else {
-			if (piece.getColor() == Color.WHITE) {
-				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-			}
-			else {
-				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-			}
+			System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
 		}
 		System.out.print(" ");
 	}
